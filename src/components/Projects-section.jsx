11d4fa84e447932d1d807c78ecf27e0d1b185ff8
Sheikh Ifty <sheikhiftekhar003@gmail.com>
2025-10-27 13:18:07 +0600

    monday now

--- src/components/Projects-section.jsx
+++ src/components/Projects-section.jsx
@@ -4,7 +4,7 @@ import { useInView } from "react-intersection-observer";
 
 export default function Projects() {
   const projects = [
-    { title: "Portfolio Website", desc: "Next.js + Tailwind portfolio.", img: "/portfolio.png", url: "https://sheikh-ifty.vercel.app/" },
+    { title: "E-commerce site", desc: "Modern e-commerce with react, next-js, Mongodb.", img: "/e-commerce.png", url: "https://glow-and-gadgets.vercel.app/" },
     { title: "My Coffee Shop", desc: "Modern e-commerce with Firebase.", img: "/coffee.jpg", url: "https://my-cafe-003.netlify.app/" },
   ];
 
